2ffcc08f82d47baa8feebd9a8698bcd05fe631f2
YaKen1 <kenzaouiyassine0@gmail.com>
2023-05-27 15:54:23 -0400

    bank

---
+++ App/.flaskenv
@@ -0,0 +1,5 @@
+FLASK_ENV=development
+FLASK_APP=main.py
+SQLALCHEMY_DATABASE_URI='mysql://yassine:root@dbase:3306/bank'
+SALT="54(&sY2S^hM@6@D^7zj@"
+SECRET_KEY='hhdiohe932#82(#$(#)(#(!*ERR'
---
+++ App/banking_system/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.secret_key = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://yassine:root@dbase:3306/bank'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SALT'] = environ.get('SALT')
+db = SQLAlchemy(app)
+
+from banking_system import routes
