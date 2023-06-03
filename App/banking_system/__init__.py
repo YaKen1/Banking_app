de96eb627212b330864df641db4e002e5d97ea5b
Yassine <111097954+YaKen1@users.noreply.github.com>
2023-06-03 23:20:43 +0100

    Update __init__.py

--- App/banking_system/__init__.py
+++ App/banking_system/__init__.py
@@ -4,7 +4,7 @@ from os import environ
 
 app = Flask(__name__)
 app.secret_key = environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://kali:root@db:3306/bank'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kali@db:3306/bank'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SALT'] = environ.get('SALT')
 db = SQLAlchemy(app)
